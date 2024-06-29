d3df3f678d277c8370089f5b26e9e44e26b35fc0
Yassine Moussafir <yassinemoussafir31@gmail.com>
2024-06-29 10:55:18 +0100

    trying new data base

--- config.py
+++ config.py
@@ -2,13 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'YassineMeriem60641939'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+mysqlconnector://root:YassineMeriem60641939@localhost/zenzilla'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://zenzilla_user:Bsxvyym1DGc36rX1rLQmjafvO6b1FpYt@dpg-cpvtimmehbks73e34ip0-a:5432/zenzilla'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SESSION_TYPE = 'filesystem'
-    SQLALCHEMY_ENGINE_OPTIONS = {
-        'pool_pre_ping': True,
-        'pool_recycle': 1800,
-        'connect_args': {
-            'connect_timeout': 10
-        }
-    }
