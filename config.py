a741c05dcd475c9d3db04a270e16fa7423784c3f
Yassine Moussafir <yassinemoussafir31@gmail.com>
2024-06-29 12:16:06 +0100

    testing config for deployement fix

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'YassineMeriem60641939'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://zenzilla_user:Bsxvyym1DGc36rX1rLQmjafvO6b1FpYt@dpg-cpvtimmehbks73e34ip0-a:5432/zenzilla'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SESSION_TYPE = 'filesystem'
