0a75e981aae72e62116952a3ced3e9bdda7b44eb
Yassine Moussafir <yassinemoussafir31@gmail.com>
2024-06-29 09:57:51 +0100

    testing connectivity to the the server

--- config.py
+++ config.py
@@ -1,9 +1,14 @@
-# config.py
-
 import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'YassineMeriem60641939'
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:YassineMeriem60641939@localhost/zenzilla'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+mysqlconnector://root:YassineMeriem60641939@localhost/zenzilla'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SESSION_TYPE = 'filesystem'
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        'pool_pre_ping': True,
+        'pool_recycle': 1800,
+        'connect_args': {
+            'connect_timeout': 10
+        }
+    }
