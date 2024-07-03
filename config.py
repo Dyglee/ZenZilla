ce56081820bb56d831cb8eb3e35d320b3c0e1246
Yassine Moussafir <yassinemoussafir31@gmail.com>
2024-07-03 12:01:31 +0100

    Config

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'YassineMeriem60641939'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Bsxvyym1DGc36rX1rLQm'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SESSION_TYPE = 'filesystem'
